7b3b4f346431611a88f32eb2cf94a6524b9fe5b0
JoWatersASC <52748951+JoWatersASC@users.noreply.github.com>
2019-08-14 17:25:57 -0400

    Add files via upload

---
+++ public/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCiBbjgErATrxftURahHOb2QnQswVPmY2c",
+    authDomain: "cherrypick-8d9c4.firebaseapp.com",
+    databaseURL: "https://cherrypick-8d9c4.firebaseio.com",
+    projectId: "cherrypick-8d9c4",
+    storageBucket: "",
+    messagingSenderId: "693361181160",
+    appId: "1:693361181160:web:c07c5b0600e86daa"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
